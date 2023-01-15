49629aa93ca8de3967d2feda7dc011da8d1c1cf4
dev1week <rondo2860@gmail.com>
2023-01-15 10:28:20 +0900

    중복 회원 가입 예외 처리

--- src/main/java/com/mysite/sbb/UserController.java
+++ src/main/java/com/mysite/sbb/UserController.java
@@ -35,9 +35,18 @@ public class UserController {
                     "2개의 패스워드가 일치하지 않습니다.");
             return "signup_form";
         }
-
+    try{
         userService.create(userCreateForm.getUsername(),
                 userCreateForm.getEmail(), userCreateForm.getPassword1());
+    }catch(DataIntegrityViolationException e){
+        e.printStackTrace();
+        bindingResult.reject("signupFailed", "이미 등록된 사용자 입니다.");
+        return "signup_form";
+    }catch (Exception e){
+        e.printStackTrace();
+        bindingResult.reject("signup Failed", e.getMessage());
+        return "signup_form";
+    }
 
         return "redirect:/question/list";
     }
